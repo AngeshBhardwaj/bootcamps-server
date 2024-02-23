f201297733c956801ad4cdc6a98ca7adaf4a318d
Angesh Bhardwaj <angeshbhardwaj@outlook.com>
2024-02-23 22:40:40 +0530

    Prettier auto enhancement

--- middleware/async.js
+++ middleware/async.js
@@ -1,6 +1,6 @@
 // A high order function to act as a wrapper so that we don't need to add try catch everywhere
 // https://www.acuriousanimal.com/blog/20180315/express-async-middleware
-const asyncHandler = fn => (req, res, next) =>
-    Promise.resolve(fn(req, res, next)).catch(next); 
+const asyncHandler = (fn) => (req, res, next) =>
+	Promise.resolve(fn(req, res, next)).catch(next);
 
 module.exports = asyncHandler;
