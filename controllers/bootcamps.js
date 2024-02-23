6d70b75a9c5cf91026f3810e5fe436e7ee2e5e60
Angesh Bhardwaj <angeshbhardwaj@outlook.com>
2024-02-23 20:51:03 +0530

    updated errorHandler to handle mongoose errors

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,8 +12,7 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({success: true, message: `List fetched successfully.`, count: bootcamps.length, data: bootcamps});
     } catch (err) {
-        console.error(`Error occured while fetching bootcamps: ${err}`.red);
-        res.status(400).json({success: false, message: `${err.message}`, data: null});
+        next(err);
     }
     
 }
@@ -36,7 +35,7 @@ exports.getBootcampById = async (req, res, next) => {
         }
         res.status(200).json({success: true, message: `Bootcamp with id ${req.params.id} fetched successfully.`, data: bootcamp});
     } catch (err) {
-        next(new ErrorResponse(`${err.message}`, 400));
+        next(err);
     }
 }
 
@@ -51,8 +50,7 @@ exports.createBootcamp = async (req, res, next) => {
         const newBootcamp = await Bootcamp.create(req.body);
         res.status(201).json({success: true, message: `New bootcamp was created successfully.`, data: newBootcamp});  
     } catch (err) {
-        console.error(`Error occured while creating bootcamp record: ${err}`.red);
-        res.status(400).json({success: false, message: `${err.message}`, data: null});
+        next(err);
     }
 }
 
@@ -71,12 +69,13 @@ exports.updateBootcampById = async (req, res, next) => {
     
         // if fetched bootcamp is null, this means the entry doesn't exists.
         if (!updatedBootcamp) {
-            return res.status(404).json({success: false, message: `Bootcamp with id ${req.params.id} does not exists.`, data: updatedBootcamp});
+            return next(
+                new ErrorResponse(`Bootcamp with id ${req.params.id} does not exists.`, 404)
+                );
         }
         res.status(200).json({success: true, message: `Bootcamp with id ${req.params.id} updated.`, data: updatedBootcamp});
     } catch (err) {
-        console.error(`Error occured while updating bootcamp record: ${err}`.red);
-        res.status(400).json({success: false, message: `${err.message}`, data: null});
+        next(err);
     }
 }
 
@@ -92,12 +91,13 @@ exports.deleteBootcampById = async (req, res, next) => {
 
         // if fetched bootcamp is null, this means the entry doesn't exists.
         if (!deletedBootcamp) {
-            return res.status(404).json({success: false, message: `Bootcamp with id ${req.params.id} does not exists.`, data: deletedBootcamp});
+            return next(
+                new ErrorResponse(`Bootcamp with id ${req.params.id} does not exists.`, 404)
+                );
         }
         res.status(200).json({success: true, message: `Bootcamp with id ${req.params.id} was DELETED successfully.`, data: deletedBootcamp});
     } catch (err) {
-        console.error(`Error occured while Deleting bootcamp record: ${err}`.red);
-        res.status(400).json({success: false, message: `${err.message}`, data: null});
+        next(err);
     }
     
 }
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,36 @@
+const ErrorResponse = require('../utils/error-response');
+
 const errorHandler = (err, req, res, next) => {
     // Log error details for DEV
+    console.error(err);     // logs the entire object, generally used when logging in log files.
     console.error(`${err.stack}`.red);
 
+    // use 'spread operator' to get a clone of actual err object with properties
+    let error = { ...err }; 
+    error.message = err.message;
+
+    // Mongoose Bad ObjectId
+    if(err.name === 'CastError') {
+        const message = `Cast to ObjectId failed for value: ${err.value}`;
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose Duplicate Key Error
+    if (err.code === 11000) {
+        const message = `Duplicate key value entered. An entry with ${JSON.stringify(err.keyValue)} already exists.`;
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose Validation Error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(e => e.message);
+        error = new ErrorResponse(message, 400);
+    }
+
     // send the response
-    res.status(err.statusCode || 500).json({
+    res.status(error.statusCode || 500).json({
         success: false, 
-        message: `${err.message}` || `Server error occured.`, 
+        message: `${error.message}` || `Server error occured.`, 
         data: null
     });
 
